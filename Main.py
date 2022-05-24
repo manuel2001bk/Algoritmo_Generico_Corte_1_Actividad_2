961708cbbbeb75fb08da7cfd074741d2c2524423
manuel2001bk <61120758+manuel2001bk@users.noreply.github.com>
2022-05-24 00:44:29 -0500

    implementado cruza, mutacion y poda.  generación de tabla historica

--- Main.py
+++ Main.py
@@ -1,18 +1,21 @@
 import math
+
 from ventana_ui import *
 
 from Paquete import Paquete
 from Individuo import Individuo
 
-from random import Random, randint, random
+from random import randint, random as random_2
+
 import random
+
+from matplotlib import pyplot as plt
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
-        self.pors_desc
         self.pors_prob_ind
         self.pors_prob_gen
         self.num_gen
@@ -20,143 +23,227 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.poblacion_maxima
         self.paquete_tipo
         self.paquete_espacio
-        self.paquete_ganancia
+        self.paquete_precio_publico
+        self.paquete_costo_envio
+        self.cantidad_paquetes
         self.cant_paquetes_label
-        self.cantidad_maxima_paquetes
         self.tam_contenedor_max
         self.lista_tipo_paquetes = []
         self.lista_paquetes = []
         self.lista_individuos = []
+        self.generaciones = []
         self.cant_max_paquetes = 0
         self.pobla_init = 0
         self.max_contenedor = 0
         self.num_div = 0
-        self.parejas = []
-        self.puntos_cruza = []
         self.porsentaje_descendencia = 0
+        self.maximo = []
+        self.promedio = []
+        self.minimo = []
         # Botones
         self.ingresar.clicked.connect(self.algoritmo)
         self.botton_ingresar_paquetes.clicked.connect(self.ingresar_paquete)
 
-    def gen__list_paquetes(self):
-        for i in range(self.cant_max_paquetes):
-            self.lista_paquetes.append(
-                randint(1, len(self.lista_tipo_paquetes)))
-        self.lista_paquetes = sorted(self.lista_paquetes)
-        print(self.lista_paquetes)
-
-    def conver_list_paquetes(self):
-        for i in range(len(self.lista_paquetes)):
-            self.lista_paquetes[i] = self.lista_tipo_paquetes[self.lista_paquetes[i]-1]
-        print(self.lista_paquetes)
-
     def gen_individuos(self):
-        print(self.lista_paquetes)
+        print("Lista de paquetes: ", self.lista_paquetes)
         for i in range(self.pobla_init):
             lista = self.lista_paquetes.copy()
             random.shuffle(lista)
             self.lista_individuos.append(Individuo(lista))
+        print("Lista de individuos generados: ", self.lista_individuos)
 
-        print(self.lista_individuos)
-
-    def cal_espacio(self):
-        for i in range(len(self.lista_individuos)):
+    def cal_espacio(self, lista):
+        for i in range(len(lista)):
             suma_espacio = 0
             suma_ganancia = 0
-            for y in range(len(self.lista_individuos[i].lista_paquetes)):
-                indiv = self.lista_individuos[i].lista_paquetes[i]
-                suma_espacio += indiv.espacio
-                suma_ganancia += indiv.ganancia
+            for y in range(len(lista[i].lista_paquetes)):
+                individuo_unic = lista[i].lista_paquetes[y]
+                suma_espacio += individuo_unic.espacio
+                suma_ganancia += individuo_unic.ganancia
                 if suma_espacio < self.max_contenedor:
-                    self.lista_individuos[i].peso_total = suma_espacio
-                    self.lista_individuos[i].ganancia_total = suma_ganancia
-                    self.lista_individuos[i].posicion_valido = y
-        print(self.lista_individuos)
+                    lista[i].peso_total = suma_espacio
+                    lista[i].ganancia_total = suma_ganancia
+                    lista[i].posicion_valido = y
+        print("Lista de individuos Despues de calculo de espacio: ", lista)
+        return lista
 
     def cal_div(self):
         self.num_div = len(self.lista_individuos)
-        self.num_div = int(self.num_div/2)
-        print("Numero de division: ", self.num_div)
+        self.num_div = math.ceil(self.num_div/2)
+        print("Numero de division 50% mejores: ", self.num_div)
 
     def crear_parejas(self):
+        parejas = []
         for i in range(self.num_div):
             for y in range(i+1, len(self.lista_individuos)):
-                print("PAREJAS: i ", i, " : y ", y)
-                self.parejas.append(
+                print("PAREJAS: i = ", i, " : y = ", y)
+                parejas.append(
                     [self.lista_individuos[i], self.lista_individuos[y]])
-        print("Lista parejas", self.parejas)
+        print("Lista parejas: ", parejas)
+        return parejas
 
     def get_hijo(self, puntos_cruza, padre1, padre2):
         hijo = []
-        for i in range(len(puntos_cruza)):
-            for y in range(puntos_cruza[i]):
-                if y < puntos_cruza[i]:
-                    hijo.append(padre1.lista_paquetes[y])
-                else:
-                    hijo.append(padre2.lista_paquetes[y])
-        return hijo
+        for i in range(puntos_cruza[0]):
+            hijo.append(padre1.lista_paquetes[i])
 
-    def gen_cruza(self):
-        puntos_cruza = random.sample(
-            range(len(self.lista_paquetes)-1), k=randint(1, 5))
-        puntos_cruza = sorted(puntos_cruza)
-        print("Puntos de cruza: ", puntos_cruza)
+        for i in range(puntos_cruza[0], puntos_cruza[1]):
+            hijo.append(padre2.lista_paquetes[i])
+
+        for i in range(puntos_cruza[1], len(padre1.lista_paquetes)):
+            hijo.append(padre1.lista_paquetes[i])
+        return Individuo(hijo)
+
+    def comprobar_repetidos(self, hijo, padre):
+        mylist = hijo.lista_paquetes
+        resultant_list = []
+
+        for element in mylist:
+            if element not in resultant_list:
+                resultant_list.append(element)
+        mylist = resultant_list
+        for element in padre.lista_paquetes:
+            if element not in mylist:
+                mylist.append(element)
+        return mylist
+
+    def generar_cruza(self, parejas):
         hijos = []
-        for i in range(len(self.parejas)):
+        for i in range(len(parejas)):
+            puntos_cruza = random.sample(
+                range(1, len(self.lista_paquetes)-2), k=2)
+            puntos_cruza = sorted(puntos_cruza)
+            print("Puntos de cruza: ", puntos_cruza)
             print("Hijo generado")
-            pareja = self.parejas[i]
+            pareja = parejas[0]
             padre1 = pareja[0]
             padre2 = pareja[1]
 
-            hijo = self.get_hijo(puntos_cruza,padre1, padre2)
+            hijo = self.get_hijo(puntos_cruza, padre1, padre2)
             print("Hijo 1: ", hijo)
-            # hijos.append(Individuo(hijo))
-            # hijo = get_hijo(puntos_cruza, padre2, padre1)
-            # print("Hijo 1: ", hijo)
-            # hijos.append(Individuo(hijo))
+            print("Padre : ", padre1)
+            print("Madre : ", padre2)
 
-    def cal_prob(self,pors):
-        probabilidad = random()
+            hijos.append(Individuo(self.comprobar_repetidos(hijo, padre2)))
+            hijo = self.get_hijo(puntos_cruza, padre2, padre1)
+            print("Hijo 1: ", hijo)
+            print("Padre : ", padre2)
+            print("Madre : ", padre1)
+            hijos.append(Individuo(self.comprobar_repetidos(hijo, padre1)))
+        return hijos
+
+    def cal_prob(self, pors):
+        probabilidad = random_2()
         if probabilidad < pors:
             return True
         else:
             return False
 
+    def mutacion_genetica(self, hijo):
+        for i in range(len(hijo.lista_paquetes)):
+            if self.cal_prob(float(self.pors_prob_gen.text())):
+                posicion = randint(i, len(hijo.lista_paquetes)-1)
+                aux = hijo.lista_paquetes[i]
+                hijo.lista_paquetes[i] = hijo.lista_paquetes[posicion]
+                hijo.lista_paquetes[posicion] = aux
+        return hijo
+
+    def mutacion_ind(self, hijos):
+        for i in range(len(hijos)):
+            if self.cal_prob(float(self.pors_prob_ind.text())):
+                hijos[i] = self.mutacion_genetica(hijos[i])
+        return hijos
+
+    def poda_mejores(self):
+        self.lista_individuos = self.lista_individuos[0:int(
+            self.poblacion_maxima.text())]
+
+    def calculo_promedio(self, lista):
+        suma = 0
+        for i in range(len(lista)):
+            suma += lista[i].ganancia_total
+        suma = suma/len(lista)
+        self.promedio.append(suma)
+
     def algoritmo(self):
         print("Ingreso a metodo analizar algoritmo")
-        self.cant_max_paquetes = int(self.cantidad_maxima_paquetes.text())
-        self.gen__list_paquetes()
-        self.conver_list_paquetes()
         self.pobla_init = int(self.poblacion_inicial.text())
         self.max_contenedor = int(self.tam_contenedor_max.text())
-        self.porsentaje_descendencia = float(self.pors_desc.text())
-        print(self.pobla_init)
         self.gen_individuos()
-        print(self.lista_paquetes)
-        self.cal_espacio()
+        self.generaciones.append(self.lista_individuos.copy())
+        # print("Lista de paquetes despues de generar individuos: ",
+        #       self.lista_paquetes)
+        self.lista_individuos = self.cal_espacio(self.lista_individuos)
+
         self.lista_individuos = sorted(
             self.lista_individuos, key=lambda genoma: genoma.peso_total, reverse=True)
         self.cal_div()
+        self.calculo_promedio(self.lista_individuos)
+        self.maximo.append(self.lista_individuos[0].ganancia_total)
+        self.minimo.append(self.lista_individuos[len(self.lista_individuos)-1].ganancia_total)
+
         print("Lista individuos ordenada", self.lista_individuos)
-        self.crear_parejas()
-        self.gen_cruza()
+        for i in range(int(self.num_gen.text())):
+            print("Generacion : ", i)
+            parejas = self.crear_parejas()
+            hijos = self.generar_cruza(parejas)
+            print("Hijos despues de cruza: ", hijos)
+            hijos = self.mutacion_ind(hijos)
+            print("Hijos despues de Mutacion: ")
+            print(hijos)
+            hijos = self.cal_espacio(hijos)
+            print("Hijos despues de calculo de ganancia: ")
+            print(hijos)
+            for y in range(len(hijos)):
+                self.lista_individuos.append(hijos[y])
 
-        # print(self.pors_desc.text())
-        # print(self.pors_prob_ind.text())
-        # print(self.pors_prob_gen.text())
-        # print(self.num_gen.text())
-        # print(self.poblacion_maxima.text())
-
-        # print(self.tam_contenedor_max.text())
+            self.lista_individuos = sorted(
+                self.lista_individuos, key=lambda genoma: genoma.peso_total, reverse=True)
+            print("Lista de poblacion antes de poda:")
+            print(self.lista_individuos)
+            self.calculo_promedio(self.lista_individuos)
+            self.maximo.append(self.lista_individuos[0].ganancia_total)
+            self.minimo.append(self.lista_individuos[len(self.lista_individuos)-1].ganancia_total)
+            self.poda_mejores()
+            print("Lista de poblacion despues de poda:")
+            print(self.lista_individuos)
+            self.generaciones.append(self.lista_individuos.copy())
+            
+        self.tabla_historial()
+    
+    
+    def tabla_historial(self):
+        x = []
+        fig = plt.figure(figsize=(10, 5))
+        fig.tight_layout()
+        plt.style.use('_mpl-gallery')
+        for i in range(len(self.maximo)):
+            x.append(i+1)
+        ax = plt.subplot(1, 1, 1)
+        ax.plot(x, self.maximo, label='Caso Máximo')
+        ax.plot(x, self.promedio, label='Caso Promedio')
+        ax.plot(x, self.minimo, label='Caso Mínimo')
+        ax.legend(loc='upper right')
+        plt.show()
 
     def ingresar_paquete(self):
         print("Ingreso a metodo generar paquete")
         tipo = self.paquete_tipo.text()
         espacio = int(self.paquete_espacio.text())
-        ganancia = int(self.paquete_ganancia.text())
+        precio_publico = int(self.paquete_precio_publico.text())
+        costo_envio = int(self.paquete_costo_envio.text())
+        cantidad_paquetes = int(self.cantidad_paquetes.text())
+        ganancia = precio_publico-costo_envio
         print("Tipo : ", tipo, " Espacio : ",
-              espacio, " Ganancia : ", ganancia)
-        self.lista_tipo_paquetes.append(Paquete(tipo, espacio, ganancia))
+              espacio, " Precio Publico : ", precio_publico)
+        print(" Costo envio : ", costo_envio,
+              " Cantidad paquetes : ", cantidad_paquetes)
+        self.lista_tipo_paquetes.append(
+            Paquete(tipo, espacio, precio_publico, costo_envio, ganancia))
+        for i in range(cantidad_paquetes):
+            self.lista_paquetes.append(
+                Paquete((tipo+str(i+1)), espacio, precio_publico, costo_envio, ganancia))
+        print(self.lista_paquetes)
         print(self.lista_tipo_paquetes)
         self.cant_paquetes_label.setText(
             "Tipos Paquetes ingresados : " + str(len(self.lista_tipo_paquetes)))
--- Paquete.py
+++ Paquete.py
@@ -1,8 +1,10 @@
 class Paquete:
-    def __init__(self, tipo, espacio, ganancia):
+    def __init__(self, tipo, espacio, precio_publico,costo_envio,ganancia):
         self.tipo = tipo
         self.espacio = espacio
+        self.precio_publico = precio_publico
+        self.costo_envio = costo_envio
         self.ganancia = ganancia
 
     def __repr__(self):
-        return repr((self.tipo, self.espacio, self.ganancia))
+        return repr((self.tipo, self.espacio, self.precio_publico, self.costo_envio, self.ganancia))
